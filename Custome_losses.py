d824f700e39d729ee50be61ae81b23a142c77065
Jose Maestre <maestrejose@hotmail.com>
2019-04-05 17:19:44 +0200

    ready for positional training phase 2

--- Custome_losses.py
+++ Custome_losses.py
@@ -4,14 +4,14 @@ import torch.nn.functional as F
 import numpy as np
 
 class FocalLoss(nn.Module):
-    def __init__(self, alpha=1, gamma=2, logits=False, reduce=True, pos_weight=None, groups=None):
+    def __init__(self, alpha=1, gamma=2, logits=False, reduce=True, pos_weight=None):
         super(FocalLoss, self).__init__()
         self.alpha = alpha
         self.gamma = gamma
         self.logits = logits
         self.reduce = reduce
         self.pos_weight = pos_weight
-        self.groups = groups
+
 
     def forward(self, inputs, targets):
         if self.logits:
@@ -27,12 +27,13 @@ class FocalLoss(nn.Module):
             return F_loss
 
 class BCE_for_non_zero(nn.Module):
-    def __init__(self, logits=False, reduce=True, pos_weight=None, alpha=1):
+    def __init__(self, logits=False, reduce=True, pos_weight=None, alpha=1, groups=None):
         super(BCE_for_non_zero, self).__init__()
         self.alpha = alpha
         self.logits = logits
         self.reduce = reduce
         self.pos_weight = pos_weight
+        self.groups = groups
 
     def forward(self, inputs, targets):
         if self.logits:
@@ -40,25 +41,48 @@ class BCE_for_non_zero(nn.Module):
         else:
             BCE_loss = F.binary_cross_entropy(inputs, targets, reduce=False, pos_weight=self.pos_weight)
 
+        if self.groups == None:
+            aux = []
+            for i in range(len(targets)):
+                aux.append(torch.sum(targets[i]))
 
-        aux = []
-        for i in range(len(targets)):
-            aux.append(torch.sum(targets[i]))
+            aux = [aux, aux]
+            aux = torch.FloatTensor(aux)
+            aux = (aux > 0).float()
+            if torch.cuda.is_available():
+                aux = aux.cuda()
 
-        aux = [aux, aux]
-        aux = torch.FloatTensor(aux)
-        aux = (aux > 0).float()
-        if torch.cuda.is_available():
-            aux = aux.cuda()
+            aux = aux.t()
 
-        aux = aux.t()
+            # print(BCE_loss)
+            # print(targets)
+            # print(aux)
+            # print(aux.size())
+            BCE_loss = BCE_loss * aux
+            BCE_loss = BCE_loss * self.alpha
+        else:
+            #print(targets)
+            #print(BCE_loss)
+            for t in range(len(targets)):
+                for group in list(set(self.groups)):
+                    sum_value = 0
+                    for i in ([i for i, x in enumerate(self.groups) if x == group]):
+                        sum_value += targets[t][i]
+                        pass
 
-        # print(BCE_loss)
-        # print(targets)
-        # print(aux)
-        # print(aux.size())
-        BCE_loss = BCE_loss * aux
-        BCE_loss = BCE_loss * self.alpha
+                    for i in ([i for i, x in enumerate(self.groups) if x == group]):
+                        #print(BCE_loss[t][i])
+                        #print((sum_value > 0).float() * self.alpha)
+                        #torch.mul(BCE_loss[t][i], torch.FloatTensor((sum_value > 0).float() * self.alpha).cuda())
+                        if group is 0:
+                            BCE_loss[t][i] = BCE_loss[t][i].cpu() * self.alpha
+                        else:
+                            BCE_loss[t][i] = BCE_loss[t][i].cpu() * ((sum_value > 0).float() * self.alpha)
+
+                        BCE_loss[t][i] = BCE_loss[t][i].cuda()
+                        pass
+                    pass
+        #print(BCE_loss)
 
         if self.reduce:
             return torch.mean(BCE_loss)
--- LR/Model_trainer_loc.py
+++ LR/Model_trainer_loc.py
@@ -47,9 +47,9 @@ if __name__ == '__main__':
         root_folder = vars(args)['root']
 
 
-    csv_train = r"C:\Users\maest\OneDrive\DTU\Semestre 4\Thesis\Code\CheXNet_aproach\Datase_stratification\a_-54.96_PADChest_train_with_loc_LR.csv"
-    csv_test = r"C:\Users\maest\OneDrive\DTU\Semestre 4\Thesis\Code\CheXNet_aproach\Datase_stratification\a_-54.96_PADChest_test_with_loc_LR.csv"
-    csv_val = r"C:\Users\maest\OneDrive\DTU\Semestre 4\Thesis\Code\CheXNet_aproach\Datase_stratification\a_-54.96_PADChest_val_with_loc_LR.csv"
+    csv_train = r"C:\Users\maest\OneDrive\DTU\Semestre 4\Thesis\Code\CheXNet_aproach\Datase_stratification\PADChest_train_LRUMDP.csv"
+    csv_test = r"C:\Users\maest\OneDrive\DTU\Semestre 4\Thesis\Code\CheXNet_aproach\Datase_stratification\PADChest_test_LRUMDP.csv"
+    csv_val = r"C:\Users\maest\OneDrive\DTU\Semestre 4\Thesis\Code\CheXNet_aproach\Datase_stratification\PADChest_val_LRUMDP.csv"
     if vars(args)['train_csv'] is not None:
         csv_train = vars(args)['train_csv']
         csv_test = vars(args)['test_csv']
@@ -64,9 +64,8 @@ if __name__ == '__main__':
 
     print(csv_train)
     print(len(pd.read_csv(csv_train).index))
-    radiographic_findings_all = ['normal', 'calcified densities', 'nodule', 'fibrotic band', 'volume loss', 'pneumothorax', 'air trapping', 'bronchiectasis', 'infiltrates', 'atelectasis', 'pleural thickening', 'pleural effusion', 'costophrenic angle blunting', 'hilar enlargement', 'cardiomegaly', 'aortic elongation', 'mediastinal enlargement', 'mass', 'thoracic cage deformation', 'vertebral degenerative changes', 'fracture', 'hemidiaphragm elevation']
     radiographic_findings_new = ['normal', 'calcified densities', 'nodule', 'fibrotic band', 'volume loss', 'pneumothorax', 'infiltrates', 'atelectasis', 'pleural thickening', 'pleural effusion', 'costophrenic angle blunting', 'cardiomegaly', 'aortic elongation', 'mediastinal enlargement', 'mass', 'thoracic cage deformation', 'fracture', 'hemidiaphragm elevation']
-    locations_labels = ['loc left', 'loc right']
+    locations_labels = ['loc left', 'loc right', 'loc upper', 'loc middle', 'loc lower', 'loc pleural', 'loc mediastinum']
 
     # Transforms
     transforms_train = transforms.Compose([Resize(512), RandomHorizontalFlip(), RandomRotation(10), ToTensor(), Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
@@ -94,37 +93,60 @@ if __name__ == '__main__':
     trainable_models = []
     trainable_models_scores = []
 
+    locations_labels = ['loc left', 'loc right', 'loc upper', 'loc middle', 'loc lower', 'loc pleural', 'loc mediastinum']
+    location_groups = [1, 1, 2, 2, 2, 0, 0]
+
     #
-    densenet = get_densenet(radiographic_findings_new, locations_labels)
-    BCE_non_zero = Custome_losses.BCE_for_non_zero(logits=True, alpha=0.5)
+    densenet = get_densenet(radiographic_findings_new, locations_labels, type=169)
+    BCE_non_zero = Custome_losses.BCE_for_non_zero(logits=True, alpha=0.1, groups=location_groups)
     sgd = optim.SGD(densenet.parameters(), lr=0.01, momentum=0.9)
     trainable_1 = Trainable_Model_LR(model=densenet, optimizer=sgd, loss_criterion_1=BCE,
                                      loss_criterion_2=BCE_non_zero, train_loader=train_dataloader,
-                                     test_loader=test_dataloader, val_loader=val_dataloader, name='DN169_BCE_SGD_A0.5',
+                                     test_loader=test_dataloader, val_loader=val_dataloader, name='BCE_A0.1',
+                                     description='alpha = 0.1')
+    trainable_models.append(trainable_1)
+    trainable_1.train_LR()
+
+    densenet = get_densenet(radiographic_findings_new, locations_labels, type=169)
+    BCE_non_zero = Custome_losses.BCE_for_non_zero(logits=True, alpha=0.25, groups=location_groups)
+    sgd = optim.SGD(densenet.parameters(), lr=0.01, momentum=0.9)
+    trainable_1 = Trainable_Model_LR(model=densenet, optimizer=sgd, loss_criterion_1=BCE,
+                                     loss_criterion_2=BCE_non_zero, train_loader=train_dataloader,
+                                     test_loader=test_dataloader, val_loader=val_dataloader, name='BCE_A0.25',
+                                     description='alpha = 0.25')
+    trainable_models.append(trainable_1)
+    trainable_1.train_LR()
+
+
+
+    densenet = get_densenet(radiographic_findings_new, locations_labels, type=169)
+    BCE_non_zero = Custome_losses.BCE_for_non_zero(logits=True, alpha=0.5, groups=location_groups)
+    sgd = optim.SGD(densenet.parameters(), lr=0.01, momentum=0.9)
+    trainable_1 = Trainable_Model_LR(model=densenet, optimizer=sgd, loss_criterion_1=BCE,
+                                     loss_criterion_2=BCE_non_zero, train_loader=train_dataloader,
+                                     test_loader=test_dataloader, val_loader=val_dataloader, name='BCE_A0.5',
                                      description='alpha = 0.5')
     trainable_models.append(trainable_1)
     trainable_1.train_LR()
 
-
-
-    densenet = get_densenet(radiographic_findings_new, locations_labels)
-    BCE_non_zero = Custome_losses.BCE_for_non_zero(logits=True, alpha=1)
+    densenet = get_densenet(radiographic_findings_new, locations_labels, type=169)
+    BCE_non_zero = Custome_losses.BCE_for_non_zero(logits=True, alpha=1, groups=location_groups)
     sgd = optim.SGD(densenet.parameters(), lr=0.01, momentum=0.9)
     trainable_1 = Trainable_Model_LR(model=densenet, optimizer=sgd, loss_criterion_1=BCE,
                                      loss_criterion_2=BCE_non_zero, train_loader=train_dataloader,
-                                     test_loader=test_dataloader, val_loader=val_dataloader, name='DN169_BCE_SGD_A1',
+                                     test_loader=test_dataloader, val_loader=val_dataloader, name='BCE_A1',
                                      description='alpha = 1')
     trainable_models.append(trainable_1)
     trainable_1.train_LR()
 
-
-
-    densenet = get_densenet(radiographic_findings_new, locations_labels)
-    BCE_non_zero = Custome_losses.BCE_for_non_zero(logits=True, alpha=2)
+    densenet = get_densenet(radiographic_findings_new, locations_labels, type=169)
+    BCE_non_zero = Custome_losses.BCE_for_non_zero(logits=True, alpha=2, groups=location_groups)
     sgd = optim.SGD(densenet.parameters(), lr=0.01, momentum=0.9)
     trainable_1 = Trainable_Model_LR(model=densenet, optimizer=sgd, loss_criterion_1=BCE,
                                      loss_criterion_2=BCE_non_zero, train_loader=train_dataloader,
-                                     test_loader=test_dataloader, val_loader=val_dataloader, name='DN169_BCE_SGD_A2',
+                                     test_loader=test_dataloader, val_loader=val_dataloader, name='BCE_A2',
                                      description='alpha = 2')
     trainable_models.append(trainable_1)
     trainable_1.train_LR()
+
+
