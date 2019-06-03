b8e325590da20a0d55f1ed1c857ab5548289d650
Jose Maestre <maestrejose@hotmail.com>
2019-06-04 01:41:07 +0200

    Heatmaps baseline try1

---
+++ Baseline/With_HM/Model_trainer.py
@@ -0,0 +1,167 @@
+import torch
+from torch import nn, optim
+import pandas as pd
+import numpy as np
+import torchvision
+from torchvision import transforms
+from torchvision.transforms import Resize, RandomRotation, ToTensor, Normalize, RandomHorizontalFlip
+from Baseline.With_HM.Trainable_model import TrainableModel
+from PADChest_DataLoading import PadChestDataset
+import Custome_losses
+import argparse
+from Baseline.With_HM import DenseNet_HM
+
+
+def get_training_weights(labels, df, data_lenght):
+    labels_count = []
+    for i in range(len(labels)):
+        labels_count.append((df.iloc[0][labels[i]]))
+
+    positive_samples_weights = []
+    classes_weights = []
+    for i in range(len(labels)):
+        count = labels_count[i]
+        positive_samples_weights.append((data_lenght - count) / count)
+    print(positive_samples_weights)
+
+    return positive_samples_weights
+
+def create_labels_info(df, labels):
+    dict_labels_info = {}
+    for l in labels:
+        dict_labels_info[l] = 0
+
+    df_length = len(df.index)
+    for i in range(df_length):
+        print(i/df_length)
+        for l in labels:
+            if df.loc[i, l] == 1:
+                dict_labels_info[l] += 1
+
+    print(dict_labels_info)
+    return pd.DataFrame(dict_labels_info, index=[0])
+
+def get_densenet(target, type=121):
+    if type is 169:
+        densenet = DenseNet_HM.DenseNet()
+        print("DenseNet_169")
+    elif type is 161:
+        densenet = torchvision.models.densenet161(pretrained=True)
+        print("DenseNet_161")
+    elif type is 201:
+        densenet = torchvision.models.densenet201(pretrained=True)
+        print("DenseNet_201")
+    else:
+        densenet = torchvision.models.densenet121(pretrained=True)
+        print("DenseNet_121")
+
+    num_ftrs = densenet.classifier.in_features
+    densenet.classifier = nn.Linear(166400, len(target))
+    densenet = densenet.cuda()
+    densenet = torch.nn.DataParallel(densenet).cuda()
+    return densenet
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--root')
+    parser.add_argument('--train_csv')
+    parser.add_argument('--test_csv')
+    parser.add_argument('--val_csv')
+    parser.add_argument('--batch_size')
+    args = parser.parse_args()
+
+    root_folder = "D:\PADChest\images8"
+    if vars(args)['root'] is not None:
+        root_folder = vars(args)['root']
+
+
+    csv_train = r"C:\Users\maest\OneDrive\DTU\Semestre 4\Thesis\Code\CheXNet_aproach\Datase_stratification\PADChest_train_LRUMDP_opacity.csv"
+    csv_test = r"C:\Users\maest\OneDrive\DTU\Semestre 4\Thesis\Code\CheXNet_aproach\Datase_stratification\PADChest_test_LRUMDP_opacity.csv"
+    csv_val = r"C:\Users\maest\OneDrive\DTU\Semestre 4\Thesis\Code\CheXNet_aproach\Datase_stratification\PADChest_val_LRUMDP_opacity.csv"
+    csv_hm = r"C:\Users\maest\OneDrive\DTU\Semestre 4\Thesis\Code\CheXNet_aproach\Datase_stratification\PADChest_hm_LRUMDP_opacity.csv"
+    if vars(args)['train_csv'] is not None:
+        csv_train = vars(args)['train_csv']
+        csv_test = vars(args)['test_csv']
+        csv_val = vars(args)['val_csv']
+        csv_hm = vars(args)['hm_csv']
+
+    batch_size = 4
+    if vars(args)['batch_size'] is not None:
+        batch_size = vars(args)['batch_size']
+        batch_size = int(batch_size)
+
+
+
+    print(csv_train)
+    print(len(pd.read_csv(csv_train).index))
+    radiographic_findings_all = ['normal', 'calcified densities', 'nodule', 'fibrotic band', 'volume loss', 'pneumothorax', 'air trapping', 'bronchiectasis', 'infiltrates', 'atelectasis', 'pleural thickening', 'pleural effusion', 'costophrenic angle blunting', 'hilar enlargement', 'cardiomegaly', 'aortic elongation', 'mediastinal enlargement', 'mass', 'thoracic cage deformation', 'vertebral degenerative changes', 'fracture', 'hemidiaphragm elevation']
+    radiographic_findings_new = ['normal', 'calcified densities', 'nodule', 'fibrotic band', 'volume loss', 'pneumothorax', 'infiltrates', 'atelectasis', 'pleural thickening', 'pleural effusion', 'costophrenic angle blunting', 'cardiomegaly', 'aortic elongation', 'mediastinal enlargement', 'mass', 'thoracic cage deformation', 'fracture', 'hemidiaphragm elevation']
+    radiographic_findings_opacity = ['opacity']
+
+
+    # Create positive samples weights
+    positive_samples_w = get_training_weights(radiographic_findings_opacity, create_labels_info(pd.read_csv(csv_train), radiographic_findings_opacity), len(pd.read_csv(csv_train).index))
+    print(np.array(positive_samples_w))
+
+    # Transforms
+    transforms_train = transforms.Compose([Resize(512), RandomHorizontalFlip(), RandomRotation(10), ToTensor(), Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+    transforms_test = transforms.Compose([Resize(512), ToTensor(), Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+
+    # Create data loaders
+    train_dataset = PadChestDataset(csv_train, radiographic_findings_opacity, root_folder, transform=transforms_train, testing=True)
+    test_dataset = PadChestDataset(csv_test, radiographic_findings_opacity, root_folder, transform=transforms_test, testing=True)
+    val_dataset = PadChestDataset(csv_val, radiographic_findings_opacity, root_folder, transform=transforms_test, testing=True)
+    hm_dataset = PadChestDataset(csv_hm, radiographic_findings_opacity, root_folder, transform=transforms_test)
+
+    train_dataloader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4,
+                                                   drop_last=True)
+    test_dataloader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=True, num_workers=4,
+                                                   drop_last=True)
+    val_dataloader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=True, num_workers=4,
+                                                   drop_last=True)
+    hm_dataloader = torch.utils.data.DataLoader(hm_dataset, batch_size=1)
+
+    # Create Backbones
+
+
+
+
+    # Loss functions
+    BCE_pos_weights = torch.FloatTensor(positive_samples_w)
+    if torch.cuda.is_available:
+        BCE_pos_weights = BCE_pos_weights.cuda()
+    BCE = nn.BCEWithLogitsLoss()
+    BCE_w = nn.BCEWithLogitsLoss(pos_weight=BCE_pos_weights)
+
+    Focal = Custome_losses.FocalLoss(logits=True)
+    Focal_w = Custome_losses.FocalLoss(pos_weight=BCE_pos_weights, logits=True)
+
+    # Trainable
+    trainable_models = []
+    trainable_models_scores = []
+
+    #
+    densenet = get_densenet(radiographic_findings_opacity, type=169)
+    sgd = optim.SGD(densenet.parameters(), lr=0.01, momentum=0.9)
+    trainable_1 = TrainableModel(model=densenet, optimizer=sgd, loss_criterion=BCE,
+                                 train_loader=train_dataloader,
+                                 test_loader=test_dataloader, val_loader=val_dataloader, hm_loader=hm_dataloader, name='Opacity_DN169_BCE_SGD')
+    trainable_models.append(trainable_1)
+    trainable_1.train()
+
+    densenet = get_densenet(radiographic_findings_opacity, type=169)
+    sgd = optim.SGD(densenet.parameters(), lr=0.01, momentum=0.9)
+    trainable_1 = TrainableModel(model=densenet, optimizer=sgd, loss_criterion=Focal,
+                                 train_loader=train_dataloader,
+                                 test_loader=test_dataloader, val_loader=val_dataloader, hm_loader=hm_dataloader, name='Opacity_DN169_FL_SGD')
+    trainable_models.append(trainable_1)
+    #trainable_1.train()
+
+
+
+
+
+
+
---
+++ Baseline/With_HM/Trainable_model.py
@@ -0,0 +1,304 @@
+import torch
+import torchvision
+import Prediction_scores
+import numpy as np
+import os
+import cv2
+
+class UnNormalize(object):
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, tensor):
+        """
+        Args:
+            tensor (Tensor): Tensor image of size (C, H, W) to be normalized.
+        Returns:
+            Tensor: Normalized image.
+        """
+        for t, m, s in zip(tensor, self.mean, self.std):
+            t.mul_(s).add_(m)
+            # The normalize code -> t.sub_(m).div_(s)
+        return tensor
+
+class TrainableModel:
+
+    def __init__(self, model, optimizer, loss_criterion, train_loader, val_loader,
+                 test_loader=None, hm_loader=None, name=None, description='', score_type='macro_roc_auc'):
+
+        self.model = model
+        self.optimizer = optimizer
+        self.loss_criterion = loss_criterion
+        self.train_loader = train_loader
+        self.val_loader = val_loader
+        self.score_type = score_type
+        self.description = description
+        self.hm_loader = hm_loader
+
+        if test_loader is not None:
+            self.test_loader = test_loader
+
+        if name is not None:
+            self.name = name
+        else:
+            self.name = ''
+
+
+    def set_name(self, name):
+        self.name = name
+
+    def log(self, str):
+        with open(self.name + '/' + self.name + '_log' + '.txt', "a") as text_file:
+            print(str, file=text_file)
+
+    def get_model_accuracy(self, model, data_loader, data_name, score_type, save_samples=False):
+        all_y = []
+        all_y_pred = []
+        with torch.no_grad():
+            for data in data_loader:
+                images, labels = data
+                images = images.cuda()
+                labels = labels.cuda()
+                # print("test images shape", images.shape)
+
+                outputs = model(images, True)
+                outputs = torch.sigmoid(outputs)
+                # print('labels:', labels)
+                # print('outputs:', outputs)
+
+                labels = labels.cpu().numpy()
+                outputs = outputs.cpu().numpy()
+
+                for a in labels:
+                    all_y.append(a)
+
+                for a in outputs:
+                    all_y_pred.append(a)
+
+        # print(all_y)
+        # print(all_y_pred)
+
+        if save_samples:
+            self.log('micro_F1: ' + str(Prediction_scores.get_micro_f1_score(all_y, all_y_pred)))
+            self.log('macro_F1 per class: ' + str(Prediction_scores.get_macro_f1_score(all_y, all_y_pred)))
+            self.log('macro_F1: ' + str(np.average(Prediction_scores.get_macro_f1_score(all_y, all_y_pred))))
+            self.log('micro_roc_auc: ' + str(Prediction_scores.get_micro_roc_auc_score(all_y, all_y_pred)))
+            self.log('macro_roc_auc per class: ' + str(Prediction_scores.get_macro_roc_auc_score(all_y, all_y_pred)))
+            self.log('macro_roc_auc: ' + str(np.average(Prediction_scores.get_macro_roc_auc_score(all_y, all_y_pred))))
+            self.log(all_y)
+            self.log(all_y_pred)
+
+        if score_type == 'micro_F1':
+            f1_score = Prediction_scores.get_micro_f1_score(all_y, all_y_pred)
+            print(self.name, data_name, 'F1:', f1_score)
+            return f1_score
+        elif score_type == 'macro_F1':
+            f1_score = Prediction_scores.get_macro_f1_score(all_y, all_y_pred)
+            print(self.name, data_name, 'F1 per class:', f1_score)
+            print(self.name, data_name, 'F1:', np.average(f1_score))
+            return np.average(f1_score)
+        elif score_type == 'micro_roc_auc':
+            score = Prediction_scores.get_micro_roc_auc_score(all_y, all_y_pred)
+            print(self.name, data_name, 'ROC_AUC_score:', score)
+            return score
+        elif score_type == 'macro_roc_auc':
+            score = Prediction_scores.get_macro_roc_auc_score(all_y, all_y_pred)
+            print(self.name, data_name, 'ROC_AUC_score per clase:', score)
+            print(self.name, data_name, 'ROC_AUC_score:', np.average(score))
+            return np.average(score)
+
+    def train(self, epochs=1, tolerance=2):
+
+        net = self.model
+        optimizer = self.optimizer
+        criterion = self.loss_criterion
+
+        epoch = 0
+
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode="min", patience=tolerance - 1)
+
+        if epochs is None:
+            epochs = -1
+
+        epochs_without_imporving = 0
+        final_net_accuracy = 0
+        epoch_losses_list = []
+        accuracies_list = []
+        decays = 0
+
+        os.mkdir(self.name)
+        self.log(self.name)
+        self.log(self.description)
+        self.log('Train criteria: ' + self.score_type)
+
+        while (epoch != epochs) & (epochs_without_imporving <= tolerance):
+            # self.log('********************************')
+            self.log('************' + 'Epoch: ' + str(epoch) + '************')
+            # self.log('********************************')
+            print('Epoch:', epoch)
+            net.train()
+            running_loss = 0.0
+            epoch_losses = []
+
+            # Learning rate decay if accuracy is not improving
+            if (epochs_without_imporving == 2) & (decays < 2):
+                decays += 1
+                net = torch.load(self.name + '/' + self.name + '.pth')
+                epochs_without_imporving = 0
+                for g in optimizer.param_groups:
+                    g['lr'] = g['lr'] / 10
+                    print('decays', decays)
+                    print('Learning rate', g['lr'])
+                    self.log('Learning rate changed to: ' + str(g['lr']))
+
+            for i, data in enumerate(self.train_loader, 0):
+                # get the inputs
+                inputs, labels = data
+                # print(labels.shape)
+                if torch.cuda.is_available():
+                    inputs = inputs.cuda()
+                    labels = labels.cuda()
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward + backward + optimize
+
+                outputs = net(inputs)
+                # print(labels.shape)
+                # print(outputs.shape)
+                loss = criterion(outputs, labels)
+                loss.backward()
+                optimizer.step()
+
+                # print statistics
+                running_loss += loss.item()
+                epoch_losses.append(loss.item())
+                if i % 100 == 99:  # print every 50 mini-batches
+                    print('[%d, %5d] loss: %.3f' %
+                          (epoch + 1, i + 1, running_loss / 100))
+                    running_loss = 0.0
+
+            net.eval()
+            temp_net_accuracy = self.get_model_accuracy(net, self.val_loader, 'val', self.score_type)
+            accuracies_list.append(temp_net_accuracy)
+            epoch_losses_list.append(np.average(np.array(epoch_losses)))
+
+            self.log('Score : ' + str(temp_net_accuracy))
+            self.log('Loss : ' + str(np.average(np.array(epoch_losses))))
+
+            if temp_net_accuracy > final_net_accuracy:
+                final_net_accuracy = temp_net_accuracy
+                torch.save(net, self.name + '/' + self.name + '_{}_{}.pth'.format(epoch, round(temp_net_accuracy, 2)))
+                torch.save(net, self.name + '/' + self.name + '.pth')
+                epochs_without_imporving = 0
+            else:
+                epochs_without_imporving += 1
+
+            epoch += 1
+
+        self.log('Scores list per epoch:')
+        self.log(str(accuracies_list))
+        self.log('Losses list per epoch:')
+        self.log(str(epoch_losses_list))
+        if self.test_loader is not None:
+            net = torch.load(self.name + '/' + self.name + '.pth')
+            net.eval()
+            self.log('Final Scores for test set')
+            final_score = self.get_model_accuracy(net, self.test_loader, 'test', self.score_type, save_samples=True)
+            self.log('Final Score for test set: ' + str(final_score))
+
+            # Heatmap creation
+
+            import matplotlib.pyplot as plt
+            import torch.nn.functional as F
+            from torchvision.transforms import functional as FVision
+            unorm = UnNormalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
+
+
+            imagelist = []
+
+            for enum, data in enumerate(self.hm_loader, 0):
+                print(enum)
+                images, labels = data
+                image_hm = images
+                images = images.cuda()
+                labels = labels.cuda()
+                net = torch.load(self.name + '/' + self.name + '.pth')
+                net.eval()
+
+                pred = net(images)
+                # get the gradient of the output with respect to the parameters of the model
+
+
+                print(pred)
+                pred[:, 0].backward()
+                print(pred.shape)
+
+                # pull the gradients out of the model
+                gradients = net.module.get_activations_gradient()
+                print('gradients', gradients.shape)
+
+                # pool the gradients across the channels
+                pooled_gradients = torch.mean(gradients, dim=[0, 2, 3])
+                print('pooled_gradients', pooled_gradients.shape)
+
+                # get the activations of the last convolutional layer
+                activations = net.module.get_activations(images).detach()
+                print('activations', activations.shape)
+
+                # weight the channels by corresponding gradients
+                print('gradients.shape[1]', gradients.shape[1])
+                for i in range(gradients.shape[1]):
+                    activations[:, i, :, :] *= pooled_gradients[i]
+
+                # average the channels of the activations
+                heatmap = torch.mean(activations, dim=1).squeeze()
+                print('heatmap', heatmap.shape)
+                # heatmap = heatmap.cpu().numpy()
+
+                # relu on top of the heatmap
+                # expression (2) in https://arxiv.org/pdf/1610.02391.pdf
+                # heatmap = np.maximum(heatmap, 0)
+                heatmap = F.relu(heatmap)
+
+                # normalize the heatmap
+                # print(torch.from_numpy(heatmap).float())
+                heatmap /= torch.max(heatmap)
+
+
+                # Print class
+                print('class', labels.cpu().numpy())
+
+                # draw the heatmap
+                heatmap = heatmap.cpu().numpy()
+                plt.matshow(heatmap.squeeze())
+                #plt.show()
+                #plt.savefig(str(enum)+'.png', bbox_inches='tight')
+
+                # heatmanp on image
+
+                #img = cv2.imread('./data/Elephant/data/05fig34.jpg')
+                print(image_hm.shape)
+                image_hm = image_hm.view(image_hm.shape[1], image_hm.shape[2], image_hm.shape[3])
+                print(image_hm.shape)
+                unorm(image_hm)
+                img = FVision.to_pil_image(image_hm, 'RGB')
+                #print(img.shape)
+                img = np.array(img)
+                print(img.shape)
+                img = img[:, :, ::-1].copy()
+                print(img.shape)
+                heatmap = cv2.resize(heatmap, (img.shape[1], img.shape[0]))
+                heatmap = np.uint8(255 * heatmap)
+                heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+                print('heatmap-shape', heatmap.shape)
+                print('img-shape', img.shape)
+                superimposed_img = heatmap * 0.4 + img
+                cv2.imwrite(str(enum) + '_img.png', img)
+                cv2.imwrite(str(enum) + '_heatmap.png', heatmap)
+                cv2.imwrite(str(enum)+'.png', superimposed_img)
+                pass
+
+
